91068f41ec04f86cfa864c3e012c1fe093acf7f4
riv2r <3237208019@qq.com>
2021-02-01 17:13:52 +0800

    添加绘制轨迹功能

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-/*
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/video.hpp>
-*/
 
 using namespace cv;
 using namespace std;
@@ -15,6 +8,7 @@ bool selectObject = false; // 用于标记是否有选取目标
 int trackObject = 0;       // 1 表示有追踪对象 0 表示无追踪对象 -1 表示追踪对象尚未计算 Camshift 所需的属性
 Rect selection;            // 保存鼠标选择的区域
 Mat image;                 // 用于缓存读取到的视频帧
+vector<Point2f> centers;   // 用于保存目标轨迹
 
 void onMouse(int event, int x, int y, int, void*) 
 {
@@ -51,13 +45,14 @@ int main(int argc, char** argv)
 {
     VideoCapture video(0);
     namedWindow("CamshiftOBJTracking");
-
+    //设置鼠标回调函数
     setMouseCallback("CamshiftOBJTracking", onMouse, 0);
     
     Mat frame, hsv, hue, mask, hist, backproj;
+    //初始化检索窗口
     Rect trackWindow;
 
-    int hsize = 16;
+    int hsize = 180;
     float hranges[] = { 0,180 };
     const float* phranges = hranges;
 
@@ -67,12 +62,15 @@ int main(int argc, char** argv)
         if (frame.empty())
             break;
         frame.copyTo(image);
+        //转换至HSV空间
         cvtColor(image, hsv, COLOR_BGR2HSV);
-        
+
         if (trackObject) 
         {
-            inRange(hsv, Scalar(0, 30, 10), Scalar(180, 256, 256), mask); //制作掩模版
-            int ch[] = { 0, 0 };
+            //制作掩模版
+            inRange(hsv, Scalar(0, 30, 10), Scalar(180, 256, 256), mask);
+            //分离hue通道
+            int ch[] = { 0, 0 }; //将hsv矩阵的第0号通道复制到hue的第0号通道
             hue.create(hsv.size(), hsv.depth());
             mixChannels(&hsv, 1, &hue, 1, ch, 1);
             
@@ -87,14 +85,18 @@ int main(int argc, char** argv)
             calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
             backproj &= mask;
             RotatedRect trackBox = CamShift(backproj, trackWindow, TermCriteria(TermCriteria::EPS | TermCriteria::COUNT, 10, 1));
-            if (trackWindow.area() <= 1) 
+            Point2f points[4];
+            trackBox.points(points);
+            for (int i = 0; i < 4; i++)
+                line(image, points[i], points[(i + 1) % 4], Scalar(255, 0, 0), 2);
+            centers.push_back(trackBox.center);
+            for (int i = 0; i < centers.size() - 1; i++)
             {
-                int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5) / 6;
-                trackWindow = Rect(trackWindow.x - r, trackWindow.y - r, trackWindow.x + r, trackWindow.y + r) & Rect(0, 0, cols, rows);
+                line(image, centers[i], centers[i + 1], Scalar(0, 255, 0), 1);
             }
-            ellipse(image, trackBox, Scalar(0, 0, 255), 3);
         }
-        if (selectObject && selection.width > 0 && selection.height > 0) {
+        if (selectObject && selection.width > 0 && selection.height > 0)
+        {
             Mat roi(image, selection);
             bitwise_not(roi, roi);
         }
