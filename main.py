2ea30de1ab485cb120c4fe03b50aeea5531cc17b
riv2r <3237208018@qq.com>
2021-03-01 15:18:31 +0800

    功能完善并补充注释

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import cv2
 import numpy as np
 
-xs, ys, ws, hs = 0, 0, 0, 0
-xo, yo = 0, 0
-selectObject = False
-trackObject = 0
+xs, ys, ws, hs = 0, 0, 0, 0 #初始化鼠标选取区域左上角坐标和长宽
+xo, yo = 0, 0 #初始化左上角原始坐标
+selectObject = False #标记是否选取了目标
+trackObject = 0 #1 表示有追踪对象 0 表示无追踪对象 -1 表示追踪对象尚未计算 Camshift 所需的属性
 
-trackPath = []
+trackPath = [] #轨迹路径列表
 
-
+# 鼠标回调函数
 def onMouse(event, x, y, flags, prams):
-    global xs, ys, ws, hs, selectObject, xo, yo, trackObject
+    global xs, ys, ws, hs, selectObject, xo, yo, trackObject, trackPath
     if selectObject == True:
         xs = min(x, xo)
         ys = min(y, yo)
@@ -23,34 +23,42 @@ def onMouse(event, x, y, flags, prams):
     elif event == cv2.EVENT_LBUTTONUP:
         selectObject = False
         trackObject = -1
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        xs, ys, ws, hs = 0, 0, 0, 0
+        xo, yo = 0, 0
+        selectObject = False
+        trackObject = 0
+        trackPath = []
 
 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
+cap = cv2.VideoCapture(0) #捕获摄像头
+ret, frame = cap.read() #获取第一帧
 cv2.namedWindow('CamshiftOBJTracking')
-cv2.setMouseCallback('CamshiftOBJTracking', onMouse)
+cv2.setMouseCallback('CamshiftOBJTracking', onMouse) #调用鼠标回调函数
+#设置结束标志，10 次迭代或至少 1 次移动
 term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
 while (True):
-    ret, frame = cap.read()
+    ret, frame = cap.read() #连续读取视频帧
+    #表示已捕获跟踪对象
     if trackObject != 0:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, np.array((0., 30., 10.)), np.array((180., 256., 255.)))
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #将BGR转换为HSV空间
+        mask = cv2.inRange(hsv, np.array((0., 30., 10.)), np.array((180., 256., 255.))) #制作掩模版
         if trackObject == -1:
             track_window = (xs, ys, ws, hs)
             mask_roi = mask[ys:ys + hs, xs:xs + ws]
             hsv_roi = hsv[ys:ys + hs, xs:xs + ws]
-            roi_hist = cv2.calcHist([hsv_roi], [0], mask_roi, [180], [0, 180])
+            #构建图像分布直方图
+            roi_hist = cv2.calcHist([hsv_roi], [0], mask_roi, [181], [0, 180])
+            #归一化处理
             cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
             trackObject = 1
+        #反向投影
         dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
         dst &= mask
+        #调用CAMshift算法
         ret, track_window = cv2.CamShift(dst, track_window, term_crit)
-        '''
-        pts = cv2.boxPoints(ret)
-        pts = np.int0(pts)
-        img2 = cv2.polylines(frame, [pts], True, 255, 2)
-        '''
+        
         x, y, w, h = track_window
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
